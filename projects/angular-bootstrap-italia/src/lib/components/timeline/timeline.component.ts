344dfaf8801ed5b06ce4c113ccbe885759eac00f
dcristinidxc <dcristini@dxc.com>
2020-06-15 10:13:59 +0200

    new timeline component

--- projects/angular-bootstrap-italia/src/lib/components/timeline/timeline.component.ts
+++ projects/angular-bootstrap-italia/src/lib/components/timeline/timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-timeline',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
+  @Input() timeHistory: {
+    datetime: string,
+    user: string,
+    changes: object
+  }[];
+
+  lastElem: {
+    datetime: string,
+    user: string,
+    changes: object,
+    changesName: string
+  };
+
+  timeline: {
+    datetime: string,
+    user: string,
+    changes: object,
+    changesName: string
+  }[];
+
   constructor() { }
 
   ngOnInit(): void {
+    const arrayLastElem = this.timeHistory[this.timeHistory.length - 1];
+    this.lastElem = {
+      datetime: arrayLastElem.datetime,
+      changes: arrayLastElem.changes,
+      user: arrayLastElem.user,
+      changesName: arrayLastElem.changes ? Object.keys(arrayLastElem.changes).join(', ') : null
+    };
+
+    this.timeHistory.pop();
+    this.timeline = this.timeHistory.map(elem => {
+      return {
+        datetime: elem.datetime,
+        user: elem.user,
+        changes: elem.changes,
+        changesName: elem.changes ? Object.keys(elem.changes).join(', ') : null
+      };
+    });
   }
 
 }
